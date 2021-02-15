6b33d39e02f903591b1aae0fb9015c4261c2ee1a
Mansour <mansurjavadi@gmail.com>
2021-02-15 10:35:13 +0330

    Clean test counter

---
+++ fluttybird/lib/home.dart
@@ -0,0 +1,14 @@
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+enum GameState { start, jumping, falling, end }
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
