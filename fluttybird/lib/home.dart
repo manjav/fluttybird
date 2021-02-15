bc30611ecf62fe8fceea26a765cb8dcd09a52c08
Mansour <mansurjavadi@gmail.com>
2021-02-15 10:38:24 +0330

    Add bird

--- fluttybird/lib/home.dart
+++ fluttybird/lib/home.dart
@@ -6,9 +6,39 @@ class HomePage extends StatefulWidget {
 enum GameState { start, jumping, falling, end }
 
 class _HomePageState extends State<HomePage> {
+  void jump() {
 
+  }
   @override
   Widget build(BuildContext context) {
-    return null;
+    return Scaffold(
+        appBar: AppBar(title: Text('y $state')),
+        body: GestureDetector(
+          onTap: jump,
+          child: Stack(
+            children: [
+              Positioned(
+                  child: Container(
+                color: Colors.green,
+              )),
+              Positioned(
+                child: Container(
+                  // transform: new Matrix4.rotationZ(birdAngle),
+                  child: MyBird(),
+                ),
+                left: 75,
+                top: birdY,
+              )
+            ],
+          ),
+        ));
+  }
+}
+
+class MyBird extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 60, height: 60, child: Image.asset("assets/images/bird.png"));
   }
 }
